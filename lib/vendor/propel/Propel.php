8b82cb2245a330bef6faf15ccb56c29e9cf72ff3
sasakir1228 <RyunosukeSasaki@lifull.com>
2018-09-18 13:44:30 +0900

    hal 1.0.22

--- lib/vendor/propel/Propel.php
+++ lib/vendor/propel/Propel.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- *  $Id: Propel.php 536 2007-01-10 14:30:38Z heltem $
+ *  $Id: Propel.php,v 1.1 2006/05/16 04:24:49 giangdt Exp $
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -29,16 +29,16 @@ include_once 'propel/adapter/DBAdapter.php';
  * This static class is used to handle Propel initialization and to maintain all of the
  * open database connections and instantiated database maps.
  *
- * @author     Hans Lellelid <hans@xmpl.rg> (Propel)
- * @author     Daniel Rall <dlr@finemaltcoding.com> (Torque)
- * @author     Magn√∫s √û√≥r Torfason <magnus@handtolvur.is> (Torque)
- * @author     Jason van Zyl <jvanzyl@apache.org> (Torque)
- * @author     Rafal Krzewski <Rafal.Krzewski@e-point.pl> (Torque)
- * @author     Martin Poeschl <mpoeschl@marmot.at> (Torque)
- * @author     Henning P. Schmiedehausen <hps@intermeta.de> (Torque)
- * @author     Kurt Schrader <kschrader@karmalab.org> (Torque)
- * @version    $Revision: 536 $
- * @package    propel
+ * @author Hans Lellelid <hans@xmpl.rg> (Propel)
+ * @author Daniel Rall <dlr@finemaltcoding.com> (Torque)
+ * @author Magn˘“ ﬁÛr Torfason <magnus@handtolvur.is> (Torque)
+ * @author Jason van Zyl <jvanzyl@apache.org> (Torque)
+ * @author Rafal Krzewski <Rafal.Krzewski@e-point.pl> (Torque)
+ * @author Martin Poeschl <mpoeschl@marmot.at> (Torque)
+ * @author Henning P. Schmiedehausen <hps@intermeta.de> (Torque)
+ * @author Kurt Schrader <kschrader@karmalab.org> (Torque)
+ * @version $Revision: 1.1 $
+ * @package propel
  */
 class Propel {
 
@@ -88,11 +88,6 @@ class Propel {
 	const LOG_DEBUG = 7;
 
 	/**
-	 * The Propel version.
-	 */
-	const VERSION = '1.2.1-dev';
-
-	/**
 	 * The db name that is specified as the default in the property file
 	 */
 	private static $defaultDBName;
@@ -123,7 +118,7 @@ class Propel {
 	private static $isInit = false;
 
 	/**
-	 * @var        Log
+	 * @var Log
 	 */
 	private static $logger = null;
 
@@ -136,14 +131,14 @@ class Propel {
 
 	/**
 	 * Cache of established connections (to eliminate overhead).
-	 * @var        array
+	 * @var array
 	 */
 	private static $connectionMap = array();
 
 	/**
 	 * initialize Propel
-	 * @return     void
-	 * @throws     PropelException Any exceptions caught during processing will be
+	 * @return void
+	 * @throws PropelException Any exceptions caught during processing will be
 	 *		 rethrown wrapped into a PropelException.
 	 */
 	public static function initialize() {
@@ -169,10 +164,10 @@ class Propel {
 				// prefixes stripped.
 				self::$configuration = $originalConf;
 		}
-
+		
 		// reset the connection map (this should enable runtime changes of connection params)
 		self::$connectionMap = array();
-
+		
 		self::initAdapters(self::$configuration);
 
 		self::$isInit = true;
@@ -194,8 +189,8 @@ class Propel {
 	 * Setup the adapters needed.  An adapter must be defined for each database connection.
 	 * Generally the adapter will be the same as the PEAR phpname; e.g. for MySQL, use the
 	 * 'mysql' adapter.
-	 * @param      array $configuration the Configuration representing the properties file
-	 * @throws     PropelException Any exceptions caught during processing will be
+	 * @param array $configuration the Configuration representing the properties file
+	 * @throws PropelException Any exceptions caught during processing will be
 	 *		 rethrown wrapped into a PropelException.
 	 */
 	private static function initAdapters($configuration) {
@@ -224,9 +219,9 @@ class Propel {
 	/**
 	 * configure propel
 	 *
-	 * @param      string $config Path (absolute or relative to include_path) to config file.
-	 * @return     void
-	 * @throws     PropelException If configuration file cannot be opened. (E_WARNING probably will also be raised in PHP)
+	 * @param string $config Path (absolute or relative to include_path) to config file.
+	 * @return void
+	 * @throws PropelException If configuration file cannot be opened. (E_WARNING probably will also be raised in PHP)
 	 */
 	public static function configure($configFile)
 	{
@@ -239,9 +234,9 @@ class Propel {
 	/**
 	 * Initialization of Propel with a properties file.
 	 *
-	 * @param      string $c The Propel configuration file path.
-	 * @return     void
-	 * @throws     PropelException Any exceptions caught during processing will be
+	 * @param string $c The Propel configuration file path.
+	 * @return void
+	 * @throws PropelException Any exceptions caught during processing will be
 	 *		 rethrown wrapped into a PropelException.
 	 */
 	public static function init($c)
@@ -253,7 +248,7 @@ class Propel {
 	/**
 	 * Determine whether Propel has already been initialized.
 	 *
-	 * @return     boolean True if Propel is already initialized.
+	 * @return boolean True if Propel is already initialized.
 	 */
 	public static function isInit()
 	{
@@ -263,8 +258,8 @@ class Propel {
 	/**
 	 * Sets the configuration for Propel and all dependencies.
 	 *
-	 * @param      array $c the Configuration
-	 * @return     void
+	 * @param array $c the Configuration
+	 * @return void
 	 */
 	public static function setConfiguration($c)
 	{
@@ -274,7 +269,7 @@ class Propel {
 	/**
 	 * Get the configuration for this component.
 	 *
-	 * @return     the Configuration
+	 * @return the Configuration
 	 */
 	public static function getConfiguration()
 	{
@@ -285,7 +280,7 @@ class Propel {
 	 * Configure the logging for this subsystem.
 	 * The logging system is only configured if there is a 'log'
 	 * section in the passed-in runtime configuration.
-	 * @return     void
+	 * @return void
 	 */
 	protected static function configureLogging() {
 		if (self::$logger === null) {
@@ -316,8 +311,8 @@ class Propel {
 	 * interface.  This interface is based on PEAR::Log, so you can also simply pass
 	 * a PEAR::Log object to this method.
 	 *
-	 * @param      object $logger The new logger to use. ([PEAR] Log or BasicLogger)
-	 * @return     void
+	 * @param object $logger The new logger to use. ([PEAR] Log or BasicLogger)
+	 * @return void
 	 */
 	public static function setLogger($logger)
 	{
@@ -328,7 +323,7 @@ class Propel {
 	 * Returns true if a logger, for example PEAR::Log, has been configured,
 	 * otherwise false.
 	 *
-	 * @return     boolean True if Propel uses logging
+	 * @return boolean True if Propel uses logging
 	 */
 	public static function hasLogger()
 	{
@@ -337,7 +332,7 @@ class Propel {
 
 	/**
 	 * Get the configured logger.
-	 * @return     object Configured log class ([PEAR] Log or BasicLogger).
+	 * @return object Configured log class ([PEAR] Log or BasicLogger).
 	 */
 	public static function logger()
 	{
@@ -349,9 +344,9 @@ class Propel {
 	 * If a logger has been configured, the logger will be used, otherwrise the
 	 * logging message will be discarded without any further action
 	 *
-	 * @param      string $message The message that will be logged.
-	 * @param      string $level The logging level.
-	 * @return     boolean True if the message was logged successfully or no logger was used.
+	 * @param string $message The message that will be logged.
+	 * @param string $level The logging level.
+	 * @return boolean True if the message was logged successfully or no logger was used.
 	 */
 	public static function log($message, $level = self::LOG_DEBUG)
 	{
@@ -387,9 +382,9 @@ class Propel {
 	 *
 	 * The database maps are "registered" by the generated map builder classes.
 	 *
-	 * @param      string $name The name of the database corresponding to the DatabaseMapto retrieve.
-	 * @return     DatabaseMap The named <code>DatabaseMap</code>.
-	 * @throws     PropelException - if database map is null or propel was not initialized properly.
+	 * @param string $name The name of the database corresponding to the DatabaseMapto retrieve.
+	 * @return DatabaseMap The named <code>DatabaseMap</code>.
+	 * @throws PropelException - if database map is null or propel was not initialized properly.
 	 */
 	public static function getDatabaseMap($name = null) {
 
@@ -405,7 +400,7 @@ class Propel {
 		// maps should be a pretty safe candidate for shared mem caching)
 
 		if (isset(self::$dbMaps[$name])) {
-			$map = self::$dbMaps[$name];
+		    $map = self::$dbMaps[$name];
 		} else {
 			$map = self::initDatabaseMap($name);
 		}
@@ -420,9 +415,9 @@ class Propel {
 	 * by calling this method and then adding the tables, etc. to teh DatabaseMap
 	 * object returned from this method.
 	 *
-	 * @param      string $name The name of the database to map.
-	 * @return     DatabaseMap The desired map.
-	 * @throws     PropelException Any exceptions caught during processing will be
+	 * @param string $name The name of the database to map.
+	 * @return DatabaseMap The desired map.
+	 * @throws PropelException Any exceptions caught during processing will be
 	 *		 rethrown wrapped into a PropelException.
 	 */
 	private static function initDatabaseMap($name)
@@ -435,7 +430,7 @@ class Propel {
 	/**
 	 * Register a MapBuilder
 	 *
-	 * @param      string $className the MapBuilder
+	 * @param string $className the MapBuilder
 	 */
 	public static function registerMapBuilder($className)
 	{
@@ -446,9 +441,9 @@ class Propel {
 	 * Returns the specified property of the given database, or the empty
 	 * string if no value is set for the property.
 	 *
-	 * @param      string $db   The name of the database whose property to get.
-	 * @param      string $prop The name of the property to get.
-	 * @return     mixed The property's value.
+	 * @param string $db   The name of the database whose property to get.
+	 * @param string $prop The name of the property to get.
+	 * @return mixed The property's value.
 	 */
 	private static function getDatabaseProperty($db, $prop)
 	{
@@ -457,18 +452,16 @@ class Propel {
 
 	/**
 	 *
-	 * @param      string $name The database name.
-	 * @return     Connection A database connection
-	 * @throws     PropelException - if no conneciton params, or SQLException caught when trying to connect.
+	 * @param string $name The database name.
+	 * @return Connection A database connection
+	 * @throws PropelException - if no conneciton params, or SQLException caught when trying to connect.
 	 */
 	public static function getConnection($name = null) {
-
 		if ($name === null) {
 			$name = self::getDefaultDB();
 		}
 
 		$con = isset(self::$connectionMap[$name]) ? self::$connectionMap[$name] : null;
-
 		if ($con === null) {
 
 			$dsn = isset(self::$configuration['datasources'][$name]['connection']) ? self::$configuration['datasources'][$name]['connection'] : null;
@@ -490,16 +483,20 @@ class Propel {
 			}
 			self::$connectionMap[$name] = $con;
 		}
-
+		/*added by Do Truong Giang, Hal Corp.
+		 * Indicate that each SQL is ujis.
+		 */
+		$stmtUjis = $con->prepareStatement("SET NAMES 'ujis'");
+		$stmtUjis->executeQuery();
 		return $con;
 	}
 
 	/**
 	 * Returns database adapter for a specific connection pool.
 	 *
-	 * @param      string $name A database name.
-	 * @return     DBAdapter The corresponding database adapter.
-	 * @throws     PropelException - if unable to find DBdapter for specified db.
+	 * @param string $name A database name.
+	 * @return DBAdapter The corresponding database adapter.
+	 * @throws PropelException - if unable to find DBdapter for specified db.
 	 */
 	public static function getDB($name = null)
 	{
@@ -515,7 +512,7 @@ class Propel {
 	/**
 	 * Returns the name of the default database.
 	 *
-	 * @return     string Name of the default DB
+	 * @return string Name of the default DB
 	 */
 	public static function getDefaultDB()
 	{
@@ -538,8 +535,8 @@ class Propel {
 	 * naming requirements you can include the class yourself
 	 * and then just use the classname instead of dot-path.
 	 *
-	 * @param      string $class dot-path to clas (e.g. path.to.my.ClassName).
-	 * @return     string unqualified classname
+	 * @param string $class dot-path to clas (e.g. path.to.my.ClassName).
+	 * @return string unqualified classname
 	 */
 	public static function import($path) {
 
@@ -551,7 +548,7 @@ class Propel {
 		}
 
 		// check if class exists
-		if (class_exists($class, true)) {
+		if (class_exists($class, false)) {
 			return $class;
 		}
 
@@ -574,7 +571,7 @@ class Propel {
 	 * This method frees any database connection handles that have been
 	 * opened by the getConnection() method.
 	 *
-	 * @return     void
+	 * @return void
 	 */
 	public static function close()
 	{
